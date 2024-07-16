08bd0c4da239ddf4a8f8067003a3df642f3a6eef
302A-24\cre <a>
2024-07-16 16:08:50 +0900

    a

--- sample/sample2.php
+++ sample/sample2.php
@@ -15,40 +15,40 @@ for ($i = 0; $i < 100; $i++) {
 // }
 
 
-// $array = array('Sun', 'Mon', 'Tue');
-// echo $array[0];    // Sun が出力される
-// echo $array[1];    // Mon が出力される
-// echo $array[2];    // Tue が出力される
+$array = array('Sun', 'Mon', 'Tue');
+echo $array[0];    // Sun が出力される
+echo $array[1];    // Mon が出力される
+echo $array[2];    // Tue が出力される
 
-// // 名前(キー)付き配列
-// $array['Sun'] = 'Sunday';
-// $array['Mon'] = 'Monday';
-// $array['Tue'] = 'Tuesday';
-// echo $array['Sun'];   // Sunday が出力される
-// echo $array['Mon'];   // Monday が出力される
-// echo $array['Tue'];   // Tuesday が出力される
-
-
-// // 名前(キー)付き配列省略形
-// $array = array(
-//     'Sun' => 'Sunday',
-//     'Mon' => 'Monday',
-//     'Tue' => 'Tuesday',   // ここのカンマ(,)は省略してもよい
-// );
-// echo $array['Sun'];   // Sunday が出力される
-// echo $array['Mon'];   // Monday が出力される
-// echo $array['Tue'];   // Tuesday が出力される
+// 名前(キー)付き配列
+$array['Sun'] = 'Sunday';
+$array['Mon'] = 'Monday';
+$array['Tue'] = 'Tuesday';
+echo $array['Sun'];   // Sunday が出力される
+echo $array['Mon'];   // Monday が出力される
+echo $array['Tue'];   // Tuesday が出力される
 
 
-// // 配列を使ったループ
-// $color_list = array("Red", "Green", "Blue");
+// 名前(キー)付き配列省略形
+$array = array(
+    'Sun' => 'Sunday',
+    'Mon' => 'Monday',
+    'Tue' => 'Tuesday',   // ここのカンマ(,)は省略してもよい
+);
+echo $array['Sun'];   // Sunday が出力される
+echo $array['Mon'];   // Monday が出力される
+echo $array['Tue'];   // Tuesday が出力される
 
-// // for
-// for ($i = 0; $i < count($color_list); $i++) {
-//     echo "$color_list[$i]\n"; // Red, Green, Blue が表示される
-// }
 
-// // 拡張for
-// foreach ($color_list as $color) {
-//     echo "$color\n"; // Red, Green, Blue が表示される                       
-// }
+// 配列を使ったループ
+$color_list = array("Red", "Green", "Blue");
+
+// for
+for ($i = 0; $i < count($color_list); $i++) {
+    echo "$color_list[$i]\n"; // Red, Green, Blue が表示される
+}
+
+// 拡張for
+foreach ($color_list as $color) {
+    echo "$color\n"; // Red, Green, Blue が表示される                       
+}
